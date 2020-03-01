b3deed6a6ffc5a0f81e814bb56e180fcf01b5f7b
Denisa Dupu <dupudenisa@gmail.com>
2020-03-01 09:33:49 -0600

    adding my prompt for employee

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,29 @@
+// TODO: Write code to define and export the Employee class
+
+const inquirer  = require9("inquirer");
+
+inquirer
+.prompt([
+    {
+        message: "What is your name?",
+        name: "name"
+
+    },
+    {
+        message: "What is your id?",
+        name: "id"
+
+    },
+    {
+        message: "What is your email?",
+        name: "email"
+    },
+
+
+
+
+
+
+
+
+])
